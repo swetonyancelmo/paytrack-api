007b882bcbf86f3e44853353f33c41ca6a39d087
Swetony <swetonyancelmo@gmail.com>
2026-02-16 16:38:21 -0300

    chore: inicializa projeto PayTrack

---
+++ src/main/java/com/swetonyancelmo/paytrack/PaytrackApplication.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.paytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swetonyancelmo/paytrack/PaytrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.paytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
