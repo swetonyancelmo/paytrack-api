4ec1ed42e4d7dea376fdc81bc43f2e02546777f7
Swetony <swetonyancelmo@gmail.com>
2026-02-16 21:25:29 -0300

    feat: adiciona método existsByEmail no repository e cria user service

--- src/main/java/com/swetonyancelmo/paytrack/repository/UserRepository.java
+++ src/main/java/com/swetonyancelmo/paytrack/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.swetonyancelmo.paytrack.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
 }
---
+++ src/main/java/com/swetonyancelmo/paytrack/service/UserService.java
@@ -0,0 +1,48 @@
+package com.swetonyancelmo.paytrack.service;
+
+import com.swetonyancelmo.paytrack.dtos.request.CreateUserDto;
+import com.swetonyancelmo.paytrack.dtos.response.UserDto;
+import com.swetonyancelmo.paytrack.exceptions.ExistingEmailException;
+import com.swetonyancelmo.paytrack.exceptions.ResourceNotFoundException;
+import com.swetonyancelmo.paytrack.mapper.ObjectMapper;
+import com.swetonyancelmo.paytrack.model.User;
+import com.swetonyancelmo.paytrack.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.logging.Logger;
+
+@Service
+public class UserService {
+
+    private Logger logger = Logger.getLogger(UserService.class.getName());
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public UserDto create(CreateUserDto dto) {
+        logger.info("Criando um usuário");
+
+        if(repository.existsByEmail(dto.email())) {
+            logger.info("O email informado já está cadastrado");
+            throw new ExistingEmailException("E-mail já cadastrado, tente novamente.");
+        }
+
+        User user = ObjectMapper.parseObject(dto, User.class);
+        User userSaved = repository.save(user);
+        return ObjectMapper.parseObject(userSaved, UserDto.class);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDto findById(Long id) {
+        logger.info("Procurando um usuário por ID");
+        User user = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado com o ID: " + id));
+
+        return ObjectMapper.parseObject(user, UserDto.class);
+    }
+}
