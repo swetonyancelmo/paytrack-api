13fe2f912740d95d0b504a82fe1c5aad8b77a65b
Swetony <swetonyancelmo@gmail.com>
2026-02-21 20:35:42 -0300

    fix: adiciona o Param informando o userId

--- src/main/java/com/swetonyancelmo/paytrack/repository/ExpenseRepository.java
+++ src/main/java/com/swetonyancelmo/paytrack/repository/ExpenseRepository.java
@@ -3,6 +3,7 @@ package com.swetonyancelmo.paytrack.repository;
 import com.swetonyancelmo.paytrack.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 
@@ -11,6 +12,6 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     Expense findByUserId(Long userId);
 
     @Query("SELECT COALESCE(SUM(e.valor), 0) FROM Expense e WHERE e.user.id = :userId")
-    BigDecimal sumValorByUserId(Long userId);
+    BigDecimal sumValorByUserId(@Param("userId") Long userId);
 
 }
